b34a85b88f982d441c91c3289d337b807629383c
fergoth <fergothew@gmail.com>
2022-06-08 12:51:17 +0300

    download books with title

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import requests
 import os
 from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
 
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError()
 
-
-
+def get_title(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    soup = BeautifulSoup(response.text, 'lxml')
+    h1 = soup.find('h1').text
+    print(h1)
+    title, author = map(lambda x : x.strip(), h1.split('::'))
+    return title
 
 def download_txt(url, filename, folder='books/'):
     """Функция для скачивания текстовых файлов.
@@ -20,41 +28,26 @@ def download_txt(url, filename, folder='books/'):
     """
     response = requests.get(url)
     response.raise_for_status()
-    try:
-        check_for_redirect(response)
-    except requests.HTTPError as error:
-        print(error)
+    check_for_redirect(response)
     if not os.path.exists(folder):
         os.makedirs(folder)
     name = sanitize_filename(filename) + '.txt'
-    fullpath = os.path.join(folder,name)
-    #if os.path.exists(fullpath): continue
+    fullpath = os.path.join(folder, name)
+    os.path.exists(fullpath):
+        return fullpath
     with open(fullpath, 'wb') as file:
         file.write(response.content)
     return fullpath
-# if not os.path.exists('books'):
-#     os.makedirs('books')
-# url = "https://tululu.org/txt.php?id={}"
-# for id in range(1,11):
-#     response = requests.get(url.format(id))
-#     response.raise_for_status()
-#     try:
-#         check_for_redirect(response)
-#     except requests.HTTPError as error:
-#         print(error)
-#         continue
-#     filename = 'books/book_{id}.txt'.format(id=id)
-#     if os.path.exists(filename): continue
-#     with open(filename, 'wb') as file:
-#         file.write(response.content)
 
-url = 'http://tululu.org/txt.php?id=1'
 
-filepath = download_txt(url, 'Алиби')
-print(filepath)  # Выведется books/Алиби.txt
-
-filepath = download_txt(url, 'Али/би', folder='books/')
-print(filepath)  # Выведется books/Алиби.txt
-
-filepath = download_txt(url, 'Али\\би', folder='txt/')
-print(filepath)  # Выведется txt/Алиби.txt
+if __name__ == "__main__":
+    for id in range(1, 11):
+        url_for_title = 'https://tululu.org/b{}/'.format(id)
+        url_for_txt = "https://tululu.org/txt.php?id={}".format(id)
+        try:
+            title = get_title(url_for_title)
+            fname  = f"{id}.{title}"
+            download_txt(url_for_txt,fname)
+        except requests.HTTPError as error:
+            print(error)
+            continue
