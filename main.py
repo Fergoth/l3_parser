588f05da1fc53d2c9a8a07b1e31470bca772741f
fergoth <fergothew@gmail.com>
2022-06-13 14:08:26 +0300

    download comments

--- main.py
+++ main.py
@@ -4,27 +4,50 @@ from urllib.parse import unquote, urlsplit, urljoin
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 
+
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError()
+
+
+def get_title(soup):
+    h1 = soup.find('h1').text
+    title, _ = map(lambda x: x.strip(), h1.split('::'))
+    return title
+
+
+def get_author(soup):
+    h1 = soup.find('h1').text
+    _, author = map(lambda x: x.strip(), h1.split('::'))
+    return author
+
+
+def get_url_image(soup):
+    image_url = soup.find('div', class_='bookimage').find('img')['src']
+    image_full_url = urljoin("https://tululu.org/", image_url)
+    return image_full_url
+
+
+def get_comments(soup):
+    raw_comments = soup.find_all('div', class_='texts')
+    comments = [i.find('span').text for i in raw_comments]
+    return comments
+
 
 def parse_book_page(id):
     """Функция для получения информации со страницы с книгой
     Args:
         id(int) : id номер книги на сайте tululu
     Returns:
-        tuple: Заголовок, автор, и урл картинки
+        soup(BeautifulSoup) : объект BeautifulSoup соответствующий странице с книгой
     """
     url = 'https://tululu.org/b{}/'.format(id)
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
-    h1 = soup.find('h1').text
-    title, author = map(lambda x : x.strip(), h1.split('::'))
-    image_url = soup.find('div' , class_='bookimage').find('img')['src']
-    image_full_url = urljoin("https://tululu.org/", image_url)
-    return title,author,image_full_url
+    return soup
+
 
 def download_image(url, filename=None, folder='images'):
     """Функция для скачивания картинок.
@@ -37,11 +60,11 @@ def download_image(url, filename=None, folder='images'):
     """
     response = requests.get(url)
     response.raise_for_status()
-    #print(unquote(urlsplit(image_full_url).path))
+    # print(unquote(urlsplit(image_full_url).path))
     file_ext = unquote(urlsplit(url).path).split('/')[-1]
     if not filename:
         filename = unquote(urlsplit(url).path).split('/')[-1]
-    print("имя файла : ",filename)
+    print("имя файла : ", filename)
     print(f"Ссылка на картинку: {url}")
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -53,6 +76,7 @@ def download_image(url, filename=None, folder='images'):
     with open(fullpath, 'wb') as file:
         file.write(response.content)
     return fullpath
+
 
 def download_txt(url, filename, folder='books/'):
     """Функция для скачивания текстовых файлов.
@@ -80,14 +104,18 @@ def download_txt(url, filename, folder='books/'):
 
 if __name__ == "__main__":
     for id in range(1, 11):
-         url_for_txt = "https://tululu.org/txt.php?id={}".format(id)
-         try:
-             title,author,image_url = parse_book_page(id);
-         except requests.HTTPError as error:
+        url_for_txt = "https://tululu.org/txt.php?id={}".format(id)
+        try:
+            soup = parse_book_page(id)
+
+        except requests.HTTPError as error:
             #print("Ошибка при парсинге страницы",error)
             continue
-         print(title,author,image_url)
-         download_image(image_url)
+        title = get_title(soup)
+        comments = get_comments(soup)
+        print(title,end = '\n\n')
+        for comment in comments:
+            print(comment)
     #         title = get_title(url_for_title)
     #         fname  = f"{id}.{title}"
     #         download_txt(url_for_txt,fname)
