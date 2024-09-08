62dee74a5d12db08c97b0daf704cafa89e261e7f
fergoth <fergothew@gmail.com>
2024-09-08 16:18:28 +0300

    added folder and skips params

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 import time
+from pathlib import Path
 from urllib.parse import urljoin, unquote, urlsplit
 
 import argparse
@@ -24,7 +25,7 @@ def get_books_ids_by_page(page_num : int, base_url: str)-> list[str]:
     books_info = soup.select(selector)
     return [book.select_one('a')['href'][2:-1] for book in books_info]
 
-def save_books_description(description: list,filename = 'description.json'):
+def save_books_description(description: list,filename = Path('description.json')):
     with open(filename,'w',encoding='UTF-8') as f:
         f.write(json.dumps(description,indent=2, ensure_ascii=False))
 
@@ -36,7 +37,7 @@ def get_book_description(description_filename = 'description.json'):
     return []
 
 
-def main(start_page:int, end_page:int):
+def main(start_page:int, end_page:int,folder:str,skip_txt:bool,skip_img:bool):
     base_url = 'https://tululu.org'
     book_ids = []
     for page_num in range(start_page,end_page):
@@ -50,38 +51,46 @@ def main(start_page:int, end_page:int):
                 print(f"Некорректный id для книги: {book_id}", error)
                 continue
             book_description = parse_book_page(soup, book_id)
-            title = book_description['title']
-            filename_for_txt = f"{book_id}.{title}.txt"
-            try:
-                fullpath_for_txt = file_full_path(filename_for_txt, 'books/')
-                book_description['book_path'] = fullpath_for_txt
-                if fullpath_for_txt:
-                    download_txt(book_id, fullpath_for_txt)
-            except requests.HTTPError as error:
-                print(f'Книги нет на сайте id: {book_id}', error)
-                continue
-            url_for_image = book_description['image_url']
-            image_filename = unquote(
-                urlsplit(url_for_image).path).split('/')[-1]
-            try:
-                fullpath_for_image = file_full_path(image_filename, 'images/')
-                if fullpath_for_image:
-                    download_image(url_for_image, fullpath_for_image)
-                book_description['image_path'] = fullpath_for_image or 'nopic.gif'
-            except requests.HTTPError as error:
-                print("Ошибка загрузки картинки", error)
-                continue
-            if book_description['book_path']:
-                books_description.append(book_description)
+            if not skip_txt:
+                title = book_description['title']
+                filename_for_txt = f"{book_id}.{title}.txt"
+                try:
+                    fullpath_for_txt = file_full_path(filename_for_txt, str(Path(folder,'books/')))
+                    book_description['book_path'] = fullpath_for_txt
+                    if fullpath_for_txt:
+                        download_txt(book_id, fullpath_for_txt)
+                except requests.HTTPError as error:
+                    print(f'Книги нет на сайте id: {book_id}', error)
+                    continue
+            else:
+                book_description['book_path'] = None
+            if not skip_img:
+                url_for_image = book_description['image_url']
+                image_filename = unquote(
+                    urlsplit(url_for_image).path).split('/')[-1]
+                try:
+                    fullpath_for_image = file_full_path(image_filename, str(Path(folder,'images/')))
+                    if fullpath_for_image:
+                        download_image(url_for_image, fullpath_for_image)
+                    book_description['image_path'] = fullpath_for_image or str(Path(folder,'images','nopic.gif'))
+                except requests.HTTPError as error:
+                    print("Ошибка загрузки картинки", error)
+                    continue
+            else:
+                book_description['image_path'] = None
+            books_description.append(book_description)
         except requests.exceptions.ConnectionError as error:
             print('Проблемы с соединением ожидаем 4 секунды', error)
             time.sleep(4)
             continue
-    save_books_description(books_description)
+    save_books_description(books_description, Path(folder,'description.json'))
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Скрипт для постраничного скачивания книг фантастики")
     parser.add_argument("--start_page", type=int,default=10,help='Первая  страница для скачивания')
     parser.add_argument("--end_page", type=int, default=11, help='Последняя страница для скачивания')
+    parser.add_argument("--dest_folder", type=str, default='downloaded', help='Путь к каталогу для скачиваемых материалов')
+    parser.add_argument("--skip_imgs", action="store_true")
+    parser.add_argument("--skip_txt", action="store_true")
     args = parser.parse_args()
-    main(args.start_page, args.end_page)
+    main(args.start_page, args.end_page,args.dest_folder,args.skip_txt,args.skip_imgs)
 
