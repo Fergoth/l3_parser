41d0e3afb34add825cc5669aa881fd70182b09f6
fergoth <fergothew@gmail.com>
2024-11-06 17:56:32 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import os
 from urllib.parse import quote
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -7,20 +8,27 @@ from more_itertools import chunked
 
 
 def rebuild():
+    books_per_page = 20
+    html_folder = 'pages'
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
+    os.makedirs(html_folder,exist_ok=True)
+
     template = env.get_template('template.html')
     with open("downloaded/description.json", 'r') as f:
         books_description = json.load(f)
+
     for book in books_description:
         book['book_path'] = quote(book['book_path'])
 
-    rendered_page = template.render({'books': chunked(books_description,2)})
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for page_num, page in enumerate(chunked(books_description,n=books_per_page)):
+        rendered_page = template.render({'books': chunked(page,2)})
+        filepath = os.path.join(html_folder,f'index{page_num+1}.html')
+        with open(filepath, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
     print('rebuilded')
 
 if __name__=='__main__':
