548735181aaa22e034f0480870c58548b288cf78
fergoth <fergothew@gmail.com>
2024-11-06 17:24:39 +0300

    Add links to books txt files.

--- render_website.py
+++ render_website.py
@@ -1,4 +1,6 @@
 import json
+from urllib.parse import quote
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
@@ -13,13 +15,16 @@ def rebuild():
     template = env.get_template('template.html')
     with open("downloaded/description.json", 'r') as f:
         books_description = json.load(f)
+    for book in books_description:
+        book['book_path'] = quote(book['book_path'])
 
     rendered_page = template.render({'books': chunked(books_description,2)})
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
     print('rebuilded')
 
-rebuild()
-server = Server()
-server.watch('template.html',rebuild)
-server.serve(root='.')
+if __name__=='__main__':
+    rebuild()
+    server = Server()
+    server.watch('template.html',rebuild)
+    server.serve(root='.')
